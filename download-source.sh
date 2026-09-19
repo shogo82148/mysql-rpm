c3766d967fb96537938b4528f20cc016be7388bf
ICHINOSE Shogo <shogo82148@gmail.com>
2026-09-19 18:39:40 +0900

    add download-source.sh script

---
+++ download-source.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# download-source.sh downloads source files from the yum repository provided by Oracle.
+# Yum repository is listed in https://dev.mysql.com/downloads/repo/yum/.
+
+set -euo pipefail
+
+ROOT=$(git rev-parse --show-toplevel)
+
+rm -rf "$ROOT/rpmbuild"
+mkdir -p "$ROOT/rpmbuild"
+
+docker run \
+  --rm \
+  -v "$ROOT/rpmbuild":/rpmbuild \
+  almalinux:10 bash -c "cd /root && dnf install -y https://dev.mysql.com/get/mysql97-community-release-el10-1.noarch.rpm && dnf download --source mysql-community-server && rpm -ivh *.src.rpm && cp -r rpmbuild /rpmbuild"
